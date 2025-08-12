7d233393462cbb6cb169c1f510506f185313aa97
cjn404 <moi404@naver.com>
2025-08-13 07:52:47 +0900

    feat: [paramtypes] Q2 구현

--- src/main/java/workbook2/paramtypes/Q2.java
+++ src/main/java/workbook2/paramtypes/Q2.java
@@ -4,14 +4,42 @@ import java.util.Scanner;
 
 public class Q2 {
     // TODO: addHundred 메서드 작성 (int[] arr의 첫 요소에 100을 더함)
+    public int addHundred(int[] arr) {
+        arr[0] += 100;
+        return arr[0];
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        // 배열 크기
         int n = sc.nextInt();
+        // 크기가 n인 배열 생성
         int[] arr = new int[n];
+        // 순서대로 정수 입력
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
-        // addHundred(arr); // TODO: addHundred 메서드 작성 후 주석 해제
+
+        Q2 q2 = new Q2();
+
+        q2.addHundred(arr); // TODO: addHundred 메서드 작성 후 주석 해제
+
         // TODO: 변경된 arr 출력
+
+        // 마지막 공백 O
+//        for (int num : arr) {
+//            System.out.print(num + " ");
+//        }
+
+        // 마지막 공백 X
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]);
+            if (i != arr.length - 1) {
+                System.out.print(" ");
+            }
+        }
+
+        sc.close();
+
     }
 }
