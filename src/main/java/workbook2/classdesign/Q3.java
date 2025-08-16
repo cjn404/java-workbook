cd6fe3a3c025462ddc0b71817ad3932d43a15ece
cjn404 <moi404@naver.com>
2025-08-17 01:52:46 +0900

    feat: [classdesign] Q3 구현

--- src/main/java/workbook2/classdesign/Q3.java
+++ src/main/java/workbook2/classdesign/Q3.java
@@ -6,9 +6,17 @@ class Book {
     String title;
     String author;
     int price;
+
     // TODO: 생성자 작성
+    public Book(String title, String author, int price) {
+        this.title = title;
+        this.author = author;
+        this.price = price;
+    }
+
     void printInfo() {
         // TODO: 정보 출력
+        System.out.println("Title: " + this.title + ", " + "Author: " + this.author + ", " + "Price: " + this.price);
     }
 }
 
@@ -21,9 +29,13 @@ public class Q3 {
         String t2 = sc.next();
         String a2 = sc.next();
         int p2 = sc.nextInt();
-        // Book b1 = new Book(t1, a1, p1); // TODO: 생성자 작성 후 주석 해제
-        // Book b2 = new Book(t2, a2, p2); // TODO: 생성자 작성 후 주석 해제
-        // b1.printInfo(); // TODO: 생성자 작성 후 주석 해제
-        // b2.printInfo(); // TODO: 생성자 작성 후 주석 해제
+
+        Book b1 = new Book(t1, a1, p1); // TODO: 생성자 작성 후 주석 해제
+        Book b2 = new Book(t2, a2, p2); // TODO: 생성자 작성 후 주석 해제
+        b1.printInfo(); // TODO: 생성자 작성 후 주석 해제
+        b2.printInfo(); // TODO: 생성자 작성 후 주석 해제
+
+        sc.close();
+
     }
 }
