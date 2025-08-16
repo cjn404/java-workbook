fc04bacb10b5df4c1ceffd65d0d421a899786c97
cjn404 <moi404@naver.com>
2025-08-17 02:55:07 +0900

    feat: [varscope] Q1 구현

--- src/main/java/workbook2/varscope/Q1.java
+++ src/main/java/workbook2/varscope/Q1.java
@@ -1,11 +1,13 @@
 package main.java.workbook2.varscope;
 
 class Sample {
-    static String classVar = "class";
-    String instanceVar = "instance";
+    static String classVar = "class";   // 클래스 변수
+    String instanceVar = "instance";    // 인스턴스 변수
     void printLocal() {
-        String localVar = "local";
+        String localVar = "local";      // 지역변수
+
         // TODO: localVar 출력
+        System.out.print(localVar + " ");
     }
 }
 
@@ -13,6 +15,10 @@ public class Q1 {
     public static void main(String[] args) {
         Sample s = new Sample();
         s.printLocal();
+
         // TODO: instanceVar와 classVar 출력
+        System.out.print(s.instanceVar + " " + s.classVar);
+
+        System.out.println();
     }
 }
