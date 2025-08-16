834e8a5c0f6ec3ac4abc5fb68d6d0a8d5ddc8e21
cjn404 <moi404@naver.com>
2025-08-17 01:43:29 +0900

    feat: [classdesign] Q2 구현

--- src/main/java/workbook2/classdesign/Q2.java
+++ src/main/java/workbook2/classdesign/Q2.java
@@ -4,10 +4,14 @@ import java.util.Scanner;
 
 class Circle {
     double r;
+
     // TODO: 생성자 작성
+    public Circle(double r) {
+        this.r = r;
+    }
     double getCircumference() {
         // TODO: 둘레 반환
-        return 0;
+        return r * 2 * Math.PI;
     }
 }
 
@@ -15,7 +19,12 @@ public class Q2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         double radius = sc.nextDouble();
-        // Circle c = new Circle(radius); // TODO: 생성자 작성 후 주석 해제
+         Circle c = new Circle(radius); // TODO: 생성자 작성 후 주석 해제
+
         // TODO: c.getCircumference() 출력
+        System.out.println(c.getCircumference());
+
+        sc.close();
+
     }
 }
