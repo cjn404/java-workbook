b67203bf8533a6f1d650fc535ffd05b60f5489c4
cjn404 <moi404@naver.com>
2025-08-17 14:38:49 +0900

    feat: [varcompare] Q3 구현

--- src/main/java/workbook2/varcompare/Q3.java
+++ src/main/java/workbook2/varcompare/Q3.java
@@ -7,6 +7,8 @@ class Student {
     int id;
     String name;
     Student(String name) {
+        this.name = name;
+        this.id = ++studentCount;
         // TODO: studentCount 증가 후 id에 할당
     }
 }
@@ -14,11 +16,14 @@ class Student {
 public class Q3 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+        int n = sc.nextInt();    // 버퍼에 '\n' 남음
         for (int i = 0; i < n; i++) {
             String name = sc.next();
             Student stu = new Student(name);
-            System.out.println(stu.name + " " + stu.id);
+            System.out.print(stu.name + " " + stu.id);
+
+            sc.close();
+
         }
     }
 }
