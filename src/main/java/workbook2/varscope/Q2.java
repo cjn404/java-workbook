8f96b4d8cd058efdaeff4bc0a9587556e885f1a9
cjn404 <moi404@naver.com>
2025-08-17 10:48:29 +0900

    feat: [varscope] Q2 구현

--- src/main/java/workbook2/varscope/Q2.java
+++ src/main/java/workbook2/varscope/Q2.java
@@ -7,6 +7,12 @@ public class Q2 {
     }
     public static void main(String[] args) {
         showLocal();
+
         // TODO: msg 변수에 접근하려고 하면 컴파일 오류
+        /** 문제: Cannot resolve symbol 'showLocal'
+         * 자바 컴파일러가 'showLocal' 인식 못 한다는 의미
+         * 메서드 showlocal() 내부의 지역 변수 msg를 외부(main 메서드)에서 출력 시도하여 발생
+         */
+         // System.out.println(showLocal);
     }
 }
