b79e60d74dd53894aab021717303b8e08ab6247f
cjn404 <moi404@naver.com>
2025-08-17 11:45:27 +0900

    feat: [varscope] Q4 구현

--- src/main/java/workbook2/varscope/Q4.java
+++ src/main/java/workbook2/varscope/Q4.java
@@ -1,17 +1,21 @@
 package main.java.workbook2.varscope;
 
 public class Q4 {
+    // static 초기화(클래스가 처초 메모리 로딩 시 한번만)
     static {
         System.out.println("static block");
     }
+    // 인스턴스 초기화(객체 생성 시마다)
     {
         System.out.println("instance block");
     }
+    // 생성자
     Q4() {
         System.out.println("constructor");
     }
 
     public static void main(String[] args) {
+        // 객체 생성 후 참조 변수 d에 저장
         Q4 d = new Q4();
     }
 }
